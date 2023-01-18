d1ea8a58e61b1dd81c6a4994dbef7163e6a1942a
Awin Abi <awin@awinawin.awin>
2023-01-18 17:25:06 +0530

    Support multiple websocket apis
    - split serverless.yml file
    - separate serverless config file for websocket API 2

--- README.md
+++ README.md
@@ -8,9 +8,20 @@ A User CRUD example with serverless framework in AWS Lambda and DynamoDB.
 
 -   Download the local version of DynamoDB from [AWS DynamoDB Download](https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.html).
 -   Unzip the contents in the `dynamodb-local` folder
--   Start dynamodb using `npm run dynamodb:start`
+-   Start dynamodb using `npm run dynamodb:start`. This requires an installation of JRE, and the `java` command.
 -   Create dynamodb tables using `npm run dynamodb:setup`, and verify the existance of the tables using `npm run dynamodb:list`
 
 ### Serverless Offline
 
 -   Run serverless offline with `npm run serve`
+
+### Multiple Websockets API
+
+-   Serverless framework allows only one api for REST, and one for websockets from a single serverless.yml file. If multiple websocket APIs needs to be deployed, it has to be in different services. For facilitiating there are 2 serverless yml files
+    -   `serverless.yml` has just the REST APIs and the websocket APIs for user, `service: sls-crud`
+    -   `serverless-device.yml` has definitions of the websocket APIs for device, in service `service: sls-crud-device`
+
+### Deploy
+
+-   Deploy using `npx sls deploy`
+-   For websockets device handlers, `npx sls deploy --config serverless-device.yml`
---
+++ src/websocket/user-ws-handler.ts
@@ -0,0 +1,51 @@
+import { DynamoDB, ApiGatewayManagementApi } from "aws-sdk";
+import { Context, APIGatewayProxyCallback, APIGatewayEvent } from "aws-lambda";
+import { APIErrorResponse, APIResponse } from "@helpers/api-response";
+
+const dynamoClient = new DynamoDB.DocumentClient();
+
+const api = new ApiGatewayManagementApi({
+    apiVersion: "2018-11-29",
+    endpoint: `https://${process.env.WS_SERVICE_URL}`,
+    maxRetries: 5,
+    httpOptions: {
+        timeout: 200,
+    },
+});
+
+export const connectDisconnectHandler = async (
+    event: APIGatewayEvent,
+    context: Context,
+    callback: APIGatewayProxyCallback
+): Promise<any> => {
+    console.log("websocket USER connect/disco function");
+    const { eventType, connectionId } = event.requestContext;
+    if (eventType == "CONNECT") {
+        console.log("Connection request ", { connectionId });
+        return { body: JSON.stringify({ status: "SUCCESS" }) };
+    } else {
+        console.log("Event", eventType, connectionId);
+    }
+};
+
+export const msgHandler = async (
+    event: APIGatewayEvent,
+    context: Context,
+    callback: APIGatewayProxyCallback
+): Promise<any> => {
+    console.log("websocket USER connect/disco function");
+    const { eventType, connectionId } = event.requestContext;
+    if (eventType == "MESSAGE") {
+        if (event.body && connectionId) {
+            console.log("Message ", JSON.parse(event.body));
+            const params = {
+                ConnectionId: connectionId,
+                Data: JSON.stringify({ message: "hello response!" }),
+            };
+            await api.postToConnection(params).promise();
+            return { body: JSON.stringify({ status: "SUCCESS" }) };
+        }
+    } else {
+        console.log("Event", eventType, connectionId);
+    }
+};
